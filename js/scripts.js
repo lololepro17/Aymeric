d31665dac7b557c30f4f2bc5b478a072c13f2a5c
lololepro17 <123694004+lololepro17@users.noreply.github.com>
2025-03-13 09:29:43 +0100

    Remove unused image files and add responsive styles for mobile and tablet views

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/* script.js */
+
+/* cette fonction gère l’ouverture/fermeture du menu hamburger sur mobile */
+function toggleMenu() {
+    const nav = document.getElementById('navLinks');
+    nav.classList.toggle('open');
+  }
+  
